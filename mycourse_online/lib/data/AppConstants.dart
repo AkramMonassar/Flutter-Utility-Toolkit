4d56e26412ccf527b59f8419336b822e57c80b5d
Akram Monassar <106633888+AkramMonassar@users.noreply.github.com>
2026-08-18 19:18:39 +0300

    Delete mycourse_online directory

--- mycourse_online/lib/data/AppConstants.dart
+++
@@ -1,6 +0,0 @@
-class AppConstants{
-  static const int RESPONSE_CODE_SUCCESS=200;
-  static const String ERROR_NO_CONNECTION_CODE="1000";
-  static const String ERROR_NO_BODY_PARSING_CODE='2000';
-
-}
